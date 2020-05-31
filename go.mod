f90a55b25f1ab98555387a01250873630eb5125c
Maykon L Fonseca <maykonlfonseca@gmail.com>
2020-05-31 17:33:56 -0300

    feat: Add gRPC server wrapper

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/maykonlf/go-devkit
 
 go 1.14
+
+require (
+	github.com/grpc-ecosystem/grpc-gateway v1.14.6
+	google.golang.org/grpc v1.29.1
+)
---
+++ grpc/server/server.go
@@ -0,0 +1,61 @@
+package server
+
+import (
+	"context"
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"net/http"
+	"strings"
+)
+
+type Server struct {
+	ctx        context.Context
+	name       string
+	mux        *runtime.ServeMux
+	gRPCServer *grpc.Server
+}
+
+func NewServer(ctx context.Context, name string) *Server {
+	return &Server{
+		name:       name,
+		ctx:        ctx,
+		mux:        runtime.NewServeMux(),
+		gRPCServer: grpc.NewServer(),
+	}
+}
+
+func (s *Server) EnableGRPCReflection() *Server {
+	reflection.Register(s.gRPCServer)
+	return s
+}
+
+func (s *Server) RegisterGRPCServerHandlersFunc(registerFunc func(server *grpc.Server)) {
+	registerFunc(s.gRPCServer)
+}
+
+func (s *Server) RegisterHTTPServerHandlersFunc(registerFunc func(ctx context.Context, mux *runtime.ServeMux)) {
+	registerFunc(s.ctx, s.mux)
+}
+
+func (s *Server) ListenAndServe(addr string) error {
+	return http.ListenAndServe(addr, s.getMuxHandler())
+}
+
+func (s *Server) getMuxHandler() http.Handler {
+	return h2c.NewHandler(http.HandlerFunc(s.httpGRPCMuxHandler), &http2.Server{})
+}
+
+func (s *Server) httpGRPCMuxHandler(w http.ResponseWriter, r *http.Request) {
+	if isGRPCRequest(r) {
+		s.gRPCServer.ServeHTTP(w, r)
+	} else {
+		s.mux.ServeHTTP(w, r)
+	}
+}
+
+func isGRPCRequest(r *http.Request) bool {
+	return r.ProtoMajor == 2 && strings.EqualFold(r.Header.Get("Content-Type"), "application/grpc")
+}
