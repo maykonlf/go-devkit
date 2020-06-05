cb32d48148974d27a9c1193585e56187acfb9bf4
Maykon L Fonseca <maykonlfonseca@gmail.com>
2020-06-04 21:53:10 -0300

    feat: add log package
    Add basic logging wrapper to uber's zap lib

---
+++ log/level.go
@@ -0,0 +1,25 @@
+package log
+
+import "go.uber.org/zap/zapcore"
+
+type Level zapcore.Level
+
+const (
+	DebugLevel = iota - 1
+	InfoLevel
+	WarnLevel
+	ErrorLevel
+	DPanicLevel
+	PanicLevel
+	FatalLevel
+)
+
+var levelMap = map[Level]zapcore.Level{
+	DebugLevel:  zapcore.DebugLevel,
+	InfoLevel:   zapcore.InfoLevel,
+	WarnLevel:   zapcore.WarnLevel,
+	ErrorLevel:  zapcore.ErrorLevel,
+	DPanicLevel: zapcore.DPanicLevel,
+	PanicLevel:  zapcore.PanicLevel,
+	FatalLevel:  zapcore.FatalLevel,
+}
