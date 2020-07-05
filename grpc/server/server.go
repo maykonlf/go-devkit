73246af69e246e696623c2d28ad14e328faebd31
Maykon L Fonseca <maykonlfonseca@gmail.com>
2020-07-05 11:06:52 -0300

    refactor: remove protocol multiplexer

--- grpc/server/server.go
+++ grpc/server/server.go
@@ -1,33 +1,25 @@
 package server
 
 import (
-	"context"
 	"fmt"
-	"net/http"
-	"strings"
-
-	"github.com/grpc-ecosystem/grpc-gateway/runtime"
 	"github.com/maykonlf/go-devkit/grpc/interceptors"
 	"github.com/maykonlf/go-devkit/log"
-	"golang.org/x/net/http2"
-	"golang.org/x/net/http2/h2c"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
+	"net"
 )
 
 type Server struct {
-	ctx        context.Context
 	name       string
-	mux        *runtime.ServeMux
+	port       int
 	gRPCServer *grpc.Server
 }
 
-func NewServer(ctx context.Context, name string) *Server {
+func NewServer(name string, port int) *Server {
 	gRPCServer := grpc.NewServer(interceptors.UnaryServerInterceptors(), interceptors.StreamServerInterceptors())
 	return &Server{
-		ctx:        ctx,
 		name:       name,
-		mux:        runtime.NewServeMux(),
+		port:       port,
 		gRPCServer: gRPCServer,
 	}
 }
@@ -37,31 +29,15 @@ func (s *Server) EnableGRPCReflection() *Server {
 	return s
 }
 
-func (s *Server) RegisterGRPCServerHandlersFunc(registerFunc func(server *grpc.Server)) {
-	registerFunc(s.gRPCServer)
-}
-
-func (s *Server) RegisterHTTPServerHandlersFunc(registerFunc func(ctx context.Context, mux *runtime.ServeMux)) {
-	registerFunc(s.ctx, s.mux)
-}
-
-func (s *Server) ListenAndServe(addr string) error {
-	log.Info(fmt.Sprintf("server listening on %s...", addr))
-	return http.ListenAndServe(addr, interceptors.HTTPLoggingMiddleware(s.getMuxHandler()))
-}
-
-func (s *Server) getMuxHandler() http.Handler {
-	return h2c.NewHandler(http.HandlerFunc(s.httpGRPCMuxHandler), &http2.Server{})
-}
-
-func (s *Server) httpGRPCMuxHandler(w http.ResponseWriter, r *http.Request) {
-	if isGRPCRequest(r) {
-		s.gRPCServer.ServeHTTP(w, r)
-	} else {
-		s.mux.ServeHTTP(w, r)
+func (s *Server) Serve() {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", s.port))
+	if err != nil {
+		log.Panic("failed to initialize listener", "port", s.port, "error", err)
 	}
+
+	log.Panic("failed to serve", "error", s.gRPCServer.Serve(lis))
 }
 
-func isGRPCRequest(r *http.Request) bool {
-	return r.ProtoMajor == 2 && strings.EqualFold(r.Header.Get("Content-Type"), "application/grpc")
+func (s *Server) GetGRPCServer() *grpc.Server {
+	return s.gRPCServer
 }
